b13b1cf9443243815873622689d6583513629286
JiRong <jirongwu81@gmail.com>
2019-12-18 17:06:45 +0800

    add clac layout

--- screens/calcPage.go
+++ screens/calcPage.go
@@ -16,8 +16,15 @@ import (
 		pec計算功能
 */
 
+const (
+	inType = iota
+	hasOperator
+	inFormula
+)
+
 type calc struct {
 	output             *widget.Label
+	formulaOutput      *widget.Label
 	bitOutput          *widget.Label
 	errOutput          *widget.Label
 	stringSrcIutput    *widget.Entry
@@ -25,12 +32,41 @@ type calc struct {
 	buttons            map[string]*widget.Button
 	radios             map[string]*widget.Radio
 	functions          map[string]func()
+	typeStatus         int
 }
 
-func (c *calc) addButtom(text string, action func()) *widget.Button {
+func (c *calc) addButton(text string, action func()) *widget.Button {
 	button := widget.NewButton(text, action)
 	c.buttons[text] = button
+	c.functions[text] = action
 	return button
+}
+
+// +-*/專用
+func (c *calc) operatorBtnFunc(text string) {
+	if c.typeStatus == inFormula {
+		temp := c.formulaOutput.Text
+		if c.output.Text != "" {
+			c.formulaOutput.SetText(temp + c.output.Text + text)
+			c.output.SetText("")
+			c.typeStatus = inFormula
+			return
+		}
+		c.formulaOutput.SetText(temp[0:len(temp)-1] + text)
+	}
+	if c.typeStatus == inType {
+		c.formulaOutput.SetText(c.formulaOutput.Text + c.output.Text + text)
+		c.output.SetText("")
+		c.typeStatus = inFormula
+	}
+}
+
+//  數字專用
+func (c *calc) numBtnFunc(text string) {
+	if c.formulaOutput.Text != "" {
+		c.typeStatus = inFormula
+	}
+	c.output.SetText(c.output.Text + text)
 }
 
 func (c *calc) addRadio(named string, text []string, isHorizontal bool) *widget.Radio {
@@ -46,6 +82,12 @@ func (c *calc) addRadio(named string, text []string, isHorizontal bool) *widget.
 
 func newCalc() *calc {
 	c := &calc{}
+	c.typeStatus = inType
+
+	c.formulaOutput = widget.NewLabel("")
+	c.formulaOutput.TextStyle.Monospace = true
+	c.formulaOutput.Alignment = fyne.TextAlignTrailing
+
 	c.output = widget.NewLabel("")
 	c.output.TextStyle.Monospace = true
 	c.output.Alignment = fyne.TextAlignTrailing
@@ -60,19 +102,41 @@ func newCalc() *calc {
 
 	c.buttons = make(map[string]*widget.Button)
 	c.radios = make(map[string]*widget.Radio)
+	c.functions = make(map[string]func())
 	return c
 }
 
 func makeCalcPageTab(win fyne.Window) fyne.CanvasObject {
 	c := newCalc()
 	win.Canvas().SetOnTypedRune(func(r rune) {
-		c.output.SetText(c.output.Text + string(r))
+		action := c.functions[string(r)]
+		if action != nil {
+			action()
+		}
+	})
+	win.Canvas().SetOnTypedKey(func(ev *fyne.KeyEvent) {
+		if ev.Name == fyne.KeyReturn || ev.Name == fyne.KeyEnter {
+			// TODO:
+			// 	計算結果
+			c.formulaOutput.SetText("")
+			c.output.SetText("")
+			c.typeStatus = inType
+		}
+		if ev.Name == fyne.KeyDelete || ev.Name == fyne.KeyBackspace {
+			if c.output.Text != "" {
+				c.output.SetText(c.output.Text[0 : len(c.output.Text)-1])
+			}
+		}
 	})
 
 	regionWidget1 := widget.NewHBox(
 		c.addRadio("digitShow", []string{"BIN", "DEC", "HEX"}, false),
 		layout.NewSpacer(),
-		c.output,
+		widget.NewVBox(
+			c.formulaOutput,
+			layout.NewSpacer(),
+			c.output,
+		),
 	)
 
 	regionWidget2 := widget.NewVBox(
@@ -84,7 +148,7 @@ func makeCalcPageTab(win fyne.Window) fyne.CanvasObject {
 		fyne.NewContainerWithLayout(
 			layout.NewVBoxLayout(),
 			c.stringSrcIutput,
-			c.addButtom("2's complement(1 byte)", func() {
+			c.addButton("2's complement(1 byte)", func() {
 				if len(c.stringSrcIutput.Text)%2 != 0 || len(c.stringSrcIutput.Text) == 0 {
 					c.stringResultOutput.SetText("Wrong bytes.")
 					return
@@ -92,7 +156,7 @@ func makeCalcPageTab(win fyne.Window) fyne.CanvasObject {
 				calcByte := []byte(c.stringSrcIutput.Text)
 				c.stringResultOutput.SetText(string(features.PEC1byte(calcByte)))
 			}),
-			c.addButtom("2's complement(2 byte)", func() {
+			c.addButton("2's complement(2 byte)", func() {
 				if len(c.stringSrcIutput.Text)%2 != 0 || len(c.stringSrcIutput.Text) == 0 {
 					c.stringResultOutput.SetText("Wrong bytes.")
 					return
@@ -104,8 +168,71 @@ func makeCalcPageTab(win fyne.Window) fyne.CanvasObject {
 		),
 	)
 
+	calcGroup := widget.NewGroup("calcator",
+		fyne.NewContainerWithLayout(
+			layout.NewGridLayout(1),
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(4),
+				c.addButton("7", func() {
+					c.numBtnFunc("7")
+				}),
+				c.addButton("8", func() {
+					c.numBtnFunc("8")
+				}),
+				c.addButton("9", func() {
+					c.numBtnFunc("9")
+				}),
+				c.addButton("+", func() {
+					c.operatorBtnFunc("+")
+				}),
+			),
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(4),
+				c.addButton("4", func() {
+					c.numBtnFunc("4")
+				}),
+				c.addButton("5", func() {
+					c.numBtnFunc("5")
+				}),
+				c.addButton("6", func() {
+					c.numBtnFunc("6")
+				}),
+				c.addButton("-", func() {
+					c.operatorBtnFunc("-")
+				}),
+			),
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(4),
+				c.addButton("1", func() {
+					c.numBtnFunc("1")
+				}),
+				c.addButton("2", func() {
+					c.numBtnFunc("2")
+				}),
+				c.addButton("3", func() {
+					c.numBtnFunc("3")
+				}),
+				c.addButton("*", func() {
+					c.operatorBtnFunc("*")
+				}),
+			),
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(4),
+				c.addButton("", func() {}),
+				c.addButton("0", func() {
+					c.numBtnFunc("0")
+				}),
+				c.addButton("", func() {}),
+				c.addButton("/", func() {
+					c.operatorBtnFunc("/")
+				}),
+			),
+		),
+	)
+
 	regionWidget3 := widget.NewHBox(
 		functionGroup,
+		calcGroup,
 	)
 
 	return widget.NewVBox(
